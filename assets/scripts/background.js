c984fae7d8e9d6c27129297761510c44b1ae9c1c
fising <fising@qq.com>
2017-03-16 18:15:28 +0800

    设置面板基本版

---
+++ assets/scripts/background.js
@@ -0,0 +1,26 @@
+({
+    createSettingWindow: function () {
+        var path = "assets/pages/popup.html";
+        var width = 500;
+        var height = 600;
+        var left = Math.round((screen.width / 2) - (width / 2));
+        var top = Math.round((screen.height / 2) - (width / 2));
+
+        chrome.windows.create({
+            url: path,
+            width: width,
+            height: height,
+            left: left,
+            top: top,
+            focused: true,
+            type: "popup"
+        });
+    },
+    init: function () {
+        var self = this;
+
+        chrome.browserAction.onClicked.addListener(function (tab) {
+            self.createSettingWindow();
+        });
+    }
+}).init();
